69612e8de55244f3a76e802c869b1b507dc065e9
MILIND JHA <81038836+SOO7H@users.noreply.github.com>
2023-10-12 00:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 const api_url = 'https://fakestoreapi.com/products';
 let specialisty = document.getElementById('specialisty-menu');
 window.onload = (event) => {
-  let cart={};
-  localStorage.setItem('cart',JSON.stringify(cart));
+  let cart = {};
+  localStorage.setItem('cart', JSON.stringify(cart));
 };
 async function getUser() {
-//   Making an API call (request)
-//   and getting the response back
+  //   Making an API call (request)
+  //   and getting the response back
   const response = await fetch(api_url);
 
   // Parsing it to JSON format[]
@@ -24,75 +24,82 @@ async function getUser() {
   console.log(name);
   console.log(name.title);
   for (let i = 0; i < data.length; i++) {
-    let a=JSON.stringify(data[i]);
+    let a = JSON.stringify(data[i]);
     console.log(data[i].title);
-    let products=document.createElement('div');
+    let products = document.createElement('div');
+    products.className="text-truncate";
+    products.onmouseover = function() {
+      this.style.backgroundColor = 'aquamarine';
+    };
+    products.onmouseout = function() {
+      this.style.backgroundColor = 'transparent';
+    };
     console.log(a);
-    const img=document.createElement('img');
-    img.src=data[i].image;
-    img.style.width="32vh";
-    img.style.height="32vh";
-    img.style.margin="auto auto";
-    const p=document.createElement('p');
-    p.innerHTML=data[i].title;
-    p.style.height="10vh";
-    const p2=document.createElement('p');
-    p2.innerHTML=data[i].description;
-    p2.style.height="15vh";
-    p2.style.fontSize="11px";
-    const rating=document.createElement('p');
-    rating.innerHTML=data[i].rating.rate;
-    const price=document.createElement('p');
-    price.innerHTML=data[i].price;
+    const img = document.createElement('img');
+    img.src = data[i].image;
+    img.style.width = "16vh";
+    img.style.height = "16vh";
+    img.style.margin = "auto auto";
+    const p = document.createElement('p');
+    p.innerHTML = data[i].title;
+    p.style.height = "auto";
+    const p2 = document.createElement('p');
+    p2.innerHTML = data[i].description;
+    p2.style.height = "auto";
+    p2.style.fontSize = "auto";
+    const rating = document.createElement('p');
+    rating.innerHTML = data[i].rating.rate;
+    const price = document.createElement('p');
+    price.innerHTML = data[i].price;
 
 
-    const button=document.createElement('button');
-    button.addEventListener("click",()=>{
-        cart=JSON.parse(localStorage.getItem('cart'));
-        cart[i]={
-          "id":data[i].id,
-          "image":data[i].image,
-          "desc":data[i].description,
-          "price":data[i].price,
-          "title":data[i].title,
-          "category":data[i].category
-        
-        };
-       
-        localStorage.setItem('cart',JSON.stringify(cart));
-        
+    const button = document.createElement('button');
+    button.addEventListener("click", () => {
+      cart = JSON.parse(localStorage.getItem('cart'));
+      cart[i] = {
+        "id": data[i].id,
+        "image": data[i].image,
+        "desc": data[i].description,
+        "price": data[i].price,
+        "title": data[i].title,
+        "category": data[i].category
+
+      };
+
+      localStorage.setItem('cart', JSON.stringify(cart));
+
     });
-    button.innerHTML="Add To Cart";
-    
-    
-    
-    products.style.width="80vh";
-    products.style.height="80vh";
-    products.style.margin="1vh 1vh";
-    products.style.alignContent="center";
-    products.style.alignItems="center";
-    products.style.border="1px solid black";
+    button.innerHTML = "Add To Cart";
+
+
+
+    products.style.width = "40vh";
+    products.style.height = "42vh";
+    products.style.margin = "1vh 1vh";
+    products.style.alignContent = "center";
+    products.style.alignItems = "center";
+    products.style.border = "1px solid black";
     products.appendChild(img);
     products.appendChild(p);
     products.appendChild(p2);
     products.appendChild(rating);
     products.appendChild(price);
     products.appendChild(button);
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
     specialisty.appendChild(products);
-    let para=document.createElement('p');
-    let para2=document.createElement('p');
-    
-    
+    let para = document.createElement('p');
+    let para2 = document.createElement('p');
+
+
   }
-  
+
 }
 
 getUser();
